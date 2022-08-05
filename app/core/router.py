14ddc2bdca630f2642f0e104a7f6c3486709629f
Viktor Viskov <vivi.skp@edu.mercantec.dk>
2022-08-05 13:17:23 +0200

    Create endpoints

--- app/core/router.py
+++ app/core/router.py
@@ -13,3 +13,20 @@ def ROUTER(SERVER: FastAPI):
     @SERVER.get("/", response_class=HTMLResponse)
     async def processor( req: Request):      
         return templates.TemplateResponse("main.jinja", {"request": req, "obj": SERVER.data, "last_update": str(SERVER.last_update).split(".")[0]})
+    
+    # get current prices
+    @SERVER.get("/api/current", response_class=JSONResponse)
+    async def processor( req: Request):      
+        return JSONResponse(SERVER.data)
+    
+    # get month prices
+    @SERVER.get("/api/month", response_class=JSONResponse)
+    async def processor( req: Request):      
+        return JSONResponse(SERVER.data)
+
+    # get week prices
+    @SERVER.get("/api/week", response_class=JSONResponse)
+    async def processor( req: Request):      
+        return JSONResponse(SERVER.data)
+    
+
